58a8567beea760326be2b79387ff4258783ea9ec
Loïc Jolivet <lo.jolivet@gmail.com>
2019-01-22 00:02:53 +0100

    Nouvelle version du site !
    Modification intro et nav, fond, mise ne page contenu et ajout d'un slider

---
+++ Portfolio_Florian_MeizaudV2/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+});
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mesSlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
